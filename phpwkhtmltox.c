a2c10181af2f9b1b07dbdcf870a6853edc567343
Matt Reiferson <mreiferson@gmail.com>
2010-09-12 13:28:36 -0400

    adding M/R INIT/SHUTDOWN

--- phpwkhtmltox.c
+++ phpwkhtmltox.c
@@ -13,17 +13,45 @@ static function_entry phpwkhtmltox_functions[] = {
     {NULL, NULL, NULL}
 };
 
+PHP_MINIT_FUNCTION(phpwkhtmltox)
+{
+    return SUCCESS;
+}
+
+PHP_MSHUTDOWN_FUNCTION(phpwkhtmltox)
+{
+    return SUCCESS;
+}
+
+PHP_RINIT_FUNCTION(phpwkhtmltox)
+{
+    return SUCCESS;
+}
+
+PHP_RSHUTDOWN_FUNCTION(phpwkhtmltox)
+{
+    return SUCCESS;
+}
+
+PHP_MINFO_FUNCTION(phpwkhtmltox)
+{
+    php_info_print_table_start();
+    php_info_print_table_row(2, "phpwkhtmltox Module", "enabled");
+    php_info_print_table_row(2, "version", wkhtmltopdf_version());
+    php_info_print_table_end();
+}
+
 zend_module_entry phpwkhtmltox_module_entry = {
 #if ZEND_MODULE_API_NO >= 20010901
     STANDARD_MODULE_HEADER,
 #endif
     PHP_PHPWKHTMLTOX_EXTNAME,
     phpwkhtmltox_functions,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
+    PHP_MINIT(phpwkhtmltox),
+    PHP_MSHUTDOWN(phpwkhtmltox),
+    PHP_RINIT(phpwkhtmltox),
+    PHP_RSHUTDOWN(phpwkhtmltox),
+    PHP_MINFO(phpwkhtmltox),
 #if ZEND_MODULE_API_NO >= 20010901
     PHP_PHPWKHTMLTOX_VERSION,
 #endif
