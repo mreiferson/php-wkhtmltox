3b6299258d599e2764b8df3c39591e7b3c34b1d0
Matt Reiferson <mreiferson@gmail.com>
2010-10-31 13:31:59 -0400

    updated test to check for multiple conversions

--- test_pdf.php
+++ test_pdf.php
@@ -1,10 +1,12 @@
 <?php
 
-wkhtmltox_convert('pdf', 
-    array('out' => '/tmp/test.pdf', 'imageQuality' => '95'), // global settings
-    array(
-        array('page' => 'http://www.visionaryrenesis.com/'),
-        array('page' => 'http://www.google.com/')
-        )); // object settings
+foreach (range(1, 4) as $i) {
+    wkhtmltox_convert('pdf', 
+        array('out' => '/tmp/test'.$i.'.pdf', 'imageQuality' => '95'), // global settings
+        array(
+            array('page' => 'http://www.visionaryrenesis.com/'),
+            array('page' => 'http://www.google.com/')
+            )); // object settings
+}
 
 ?>
